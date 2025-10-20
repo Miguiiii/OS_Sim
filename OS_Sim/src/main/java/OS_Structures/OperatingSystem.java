c0969b8d8e8175d6eab6d4449938f7b55798cbc9
Miguel <Miguel@DESKTOP-AIUBK8A>
2025-10-20 10:45:43 -0400

    OS Changes

--- OS_Sim/src/main/java/OS_Structures/OperatingSystem.java
+++ OS_Sim/src/main/java/OS_Structures/OperatingSystem.java
@@ -62,6 +62,12 @@ public class OperatingSystem {
         counterThread.start();
     }
     
+    private void manageSchedule() {
+        
+    }
     
+    private void manageMemory() {
+        
+    }
     
 }
